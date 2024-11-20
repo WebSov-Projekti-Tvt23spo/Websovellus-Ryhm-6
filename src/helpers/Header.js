170ca4da546cdab377c37fde51e3bd4d193fb19d
markalterego <122254234+markalterego@users.noreply.github.com>
2024-11-20 16:10:19 +0200

    combined Header+HomeHeader

--- src/helpers/Header.js
+++ src/helpers/Header.js
@@ -1,11 +1,29 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function Header() {
+    const [user, setUser] = useState('*insert name*');
     const [search, setSearch] = useState('');
+    const url = useLocation();
+
+    const renderHeaderByUrl = () => {
+        if (url.pathname === '/') {
+            return <h1>Welcome {user}</h1>;
+        } else {
+            return <a href='./'>Return</a>;
+        }
+    }
+
+    const transportSearch = () => {
+        const s = search;
+        setSearch('');
+        console.log('Returning value for search: \'' + s + '\'');
+        return s;
+    };
 
     return ( 
         <div className='headerContainer'>
-            <a href='./'>Return</a>
+            {renderHeaderByUrl()}
             <form>
                 <input 
                     placeholder='Search'
@@ -14,7 +32,7 @@ export default function Header() {
                     onKeyDown={e => {
                         if (e.key === 'Enter') {
                             e.preventDefault();
-                            setSearch('');
+                            transportSearch();
                         }    
                     }}
                 />
