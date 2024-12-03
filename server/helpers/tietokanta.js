4b6233bf368ad6836d007c1f18aa10a8a5f1a835
markalterego <jesse.paukkunen@gmail.com>
2024-12-03 12:52:33 +0200

    routed user table + added generateEnv.js

--- server/helpers/tietokanta.js
+++ server/helpers/tietokanta.js
@@ -9,7 +9,7 @@ const openDb = () => {
     const pool = new Pool ({
      user: process.env.DB_USER,
      host: process.env.DB_HOST,
-     database: process.env.DB_NAME,
+     database: process.env.NODE_ENV === 'development' ? process.env.DB_NAME : process.env.TEST_DB_NAME, // devStart uses movie db and testStart uses test db
      password: process.env.DB_PASSWORD,
      port: process.env.DB_PORT
     })
