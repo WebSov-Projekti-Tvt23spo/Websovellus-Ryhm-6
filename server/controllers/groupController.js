0d424db4c536904ad13711955ba3269e16a63db2
markalterego <122254234+markalterego@users.noreply.github.com>
2024-12-09 22:44:14 +0200

    updated movie.sql and made group creation possible

--- server/controllers/groupController.js
+++ server/controllers/groupController.js
@@ -3,16 +3,33 @@ import { getGroups, getGroup, createGroup, deleteGroup } from '../models/group.j
 const fetchGroups = async (req, res) => {
     try {
         const groups = await getGroups()
-        return res.status(200).json(groups)
+
+        const updatedGroups = groups.map(group => { // go through all groups
+            if (group.groupImage) { // if row contains an image
+                const base64Image = `data:image/jpeg;base64,${group.groupImage.toString('base64')}`; // change image to base64 type
+                return { ...group, groupImage: base64Image }; // update image for the group
+            }
+            return group; // if groupImage is not defined, return group data as is
+        });
+
+        console.log(groups);
+
+        return res.status(200).json(updatedGroups);
     } catch (error) {
         return res.status(500).json({ error: error.message })
     }
 }
 
 const fetchGroup = async(req, res) => {
-    const { groupId } = req.params
+    const { groupId } = req.params;
     try {
-        const group = await getGroup(groupId)
+        const group = await getGroup(groupId);
+
+        if (group.groupImage) { // if image exists
+            const base64Image = `data:image/jpeg;base64,${group.groupImage.toString('base64')}`; // change image to base64 type
+            group.groupImage = base64Image; // update image for the group
+        }
+
         return res.status(200).json(group)
     } catch (error) {
         return res.status(500).json({ error: error.message})
@@ -22,7 +39,8 @@ const fetchGroup = async(req, res) => {
 const addGroup = async(req,res) => {
     const { groupName, groupDescription, groupImage, owner } = req.body
     try {
-        const groupId = await createGroup(groupName, groupDescription, groupImage, owner)
+        const groupImageData = Buffer.from(groupImage.replace(/^data:image\/\w+;base64,/, ""), 'base64'); // save binary of groupImage to groupImageData
+        const groupId = await createGroup(groupName, groupDescription, groupImageData, owner)
         return res.status(200).json({ id: groupId })
     } catch (error) {
         return res.status(500).json({ error: error.message })
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import movieRouter from './routers/movieRouter.js';
 import userRouter from './routers/userRouter.js';
 import groupRouter from './routers/groupRouter.js'
@@ -10,14 +11,11 @@ const port = process.env.PORT; // gets port from env file
 const app = express();
 app.use(express.json()); // allows client to send json data
 app.use(express.urlencoded({extended: false})); // use curl through url
-
-/* these have yet to be implemented
-app.use(cors()); // allows testing from different origin
-*/
+app.use(cors()); // allows e.g. localhost:3000 and localhost:3001 to communicate
 
 app.use('/', movieRouter);
 app.use('/user', userRouter);
-app.use('/group', groupRouter)
+app.use('/group', groupRouter);
 
 app.use((err,res) => { // error handling
     const statusCode = err.statusCode || 500;
